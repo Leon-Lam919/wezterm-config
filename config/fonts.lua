aba99a8ebf22cb3078a03dd5ad868efadbd5a6d6
Leon Lam <leonlam34@gmail.com>
2024-11-19 19:14:28 -0600

    adjusted config files

--- config/fonts.lua
+++ config/fonts.lua
@@ -10,7 +10,7 @@ return {
       family = font_family,
       weight = 'Medium',
    }),
-   font_size = font_size,
+   font_size = 12,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
